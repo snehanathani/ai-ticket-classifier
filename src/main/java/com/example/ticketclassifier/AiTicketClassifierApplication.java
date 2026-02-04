5c0bf4e3cf827e68f266cf37a3b074876ff3c347
sanaa <snehanathani@gmail.com>
2026-02-04 07:01:23 +0530

    initial commit with spring Intializer

---
+++ src/main/java/com/example/ticketclassifier/AiTicketClassifierApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_ticket_classifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTicketClassifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiTicketClassifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ai_ticket_classifier/AiTicketClassifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_ticket_classifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTicketClassifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
